5769d0185f4684d8ae1661ac882d1e2f6ae4dac4
silverberrybird <bgv930331@gmail.com>
2024-08-30 18:58:51 +0530

    30-08-24

---
+++ function_in_list.py
@@ -0,0 +1,26 @@
+l="abcsdcasd"
+print(l)
+print(max(l))
+print(min(l))
+print(min(2,3))
+print(sorted(l))
+
+# l=[1,2,3,4,5,6,7,8,9,10]
+# print(l)
+# print(type(l))
+# print(len(l))
+# print(max(l))
+# print(min(l))
+# print(sum(l))
+# print(sum(l,100))
+#
+# s=['a','b','c','d','e','f','g','h','i','j']
+# print(s)
+# # print(sum(s))
+# print(max(s))
+# print(min(s))
+#
+# c=[2+2j,4+5j,5+6j,6+6j,7+7j,9+1j]
+# print(sum(c))
+# print(max(c))
+# print(min(c))
