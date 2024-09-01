bfdcd5fa2dc1e426416781df2d498a5e51094918
silverberrybird <bgv930331@gmail.com>
2024-09-01 17:25:59 +0530

    30-08-24

--- function_in_list.py
+++ function_in_list.py
@@ -1,9 +1,11 @@
-l="abcsdcasd"
-print(l)
-print(max(l))
-print(min(l))
-print(min(2,3))
-print(sorted(l))
+# l="abcsdcasd"
+# print(l)
+# print(max(l))
+# print(min(l))
+# print(min(2 , 3))
+# print(sorted(l))
+# print(sorted(l,reverse=True))
+
 
 # l=[1,2,3,4,5,6,7,8,9,10]
 # print(l)
@@ -24,3 +26,138 @@ print(sorted(l))
 # print(sum(c))
 # print(max(c))
 # print(min(c))
+# l=[100,455,600,67,900]
+# print(l)
+# a=sorted(l)
+# print(a)
+# print(l)
+# l=['aaa','av','cda','abcd','aad']
+# print(l)
+# a=sorted(l)
+# print(sorted(l))
+# print(sorted(l,reverse=True))
+# print(sorted(a,key=len))
+# l=[[1,2],[3,4],[1,2,1],[1,3],[1,2,2]]
+# print(l)
+# print(sorted(l))
+# print(sorted(l,reverse=True,key=len))
+
+# list function
+# index function
+l=[10,20,3,2,4,5]
+print(l.index(3))
+# print(l.index("3"))
+# print(l.index(8))
+#
+# l=[0,1,True,False]
+# print(l.index(True))
+# print(l.index(False))
+#
+# l=[2,1,3,2,-1]
+# print(l.index(2))
+# print(l.index(2,1))
+# print(l.index(2,1,3))
+#
+# l = [[1, 2], [3, 4], [1, 2, 1], [1, 3], [1, 2, 2]]
+# print(l.index([1,2,1]))
+#
+# l=[{1,2,1},{1,2,1,1},{2,1,1}]
+# print(l.index({1,2,1}))
+# print(l.index({1,2,1,1}))
+
+# append function
+# l=[1,2,3,4,5]
+# print(l.append(6))
+# print(l)
+# print(l.append(7,78))
+# l.append([10,20,30,40,50])
+# print(l)
+# l.append("abc")
+# print(l)
+
+# extend function
+# l="abc"
+# l1=[1,2,3,4,5]
+# l1.extend(l)
+# print(l1)
+# l1.extend(10)
+# print(l1)
+# l1.extend(['a','b','c'])
+# print(l1)
+
+# insert function
+# l=[1,2,3,4,5]
+# l.insert(1,"abc")
+# print(l)
+# l.insert(-2,10)
+# print(l)
+# l.insert(-1,20)
+# print(l)
+# l.insert(0,30)
+# print(l)
+# l.insert(4,40)
+# print(l)
+# l.insert(-5,50)
+# print(l)
+# l.insert(6,60)
+# print(l)
+# l.insert(6,[70,80,90])
+# print(l)
+
+# reverse function
+# l=[10,20,3,2,4,5]
+# l.reverse()
+# print(l)
+# print(reversed(l))
+
+# sort function
+# l=[1,5,6,2,3,4,8,7]
+# l.sort()
+# print(l)
+# l.sort(reverse=True)
+# print(l)
+# l=['aa','abc','aab','ab','abcd']
+# l.sort(key=len)
+# print(l)
+
+# count function
+# l=[1,2,1,3,4,1,5,6,3]
+# print(l.count(1))
+# print(l.count(2))
+# print(l.count(7))
+
+# pop function
+# l=[10,20,3,2,4,5]
+# print(l.pop())
+# print(l)
+# print(l.pop(2))
+# print(l)
+# print(l.pop(8))
+# print(l.pop(2,3))
+# print(l.pop([2:]))
+
+# remove function
+# l=[10,20,30,40,50]
+# l.remove(20)
+# print(l)
+# print(l.remove(200))
+# print(l)
+
+# clear function
+# l=[10,20,3,2,4,5]
+# l.clear()
+# print(l)
+
+# del keyword
+# a=10
+# print(a)
+# del a
+# print(a)
+#
+# l=[10,20,3,2,4,5]
+# del l[1]
+# print(l)
+# del l[1:3]
+# print(l)
+# del l
+# print(l)
